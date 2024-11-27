e89a061031e095221e2637f028daa1062206015d
Tim Proctor <tim@blowfish.xyz>
2024-11-27 07:31:35 -0600

    add new next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+    ...nextConfig,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                'async_hooks': false,
+            };
+        }
+        return config;
+    },
+};
